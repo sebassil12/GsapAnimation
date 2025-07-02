55830bc967043aae2e5069a9e277463bf8ca849c
sebassil12 <anddysilva15@gmail.com>
2025-07-01 23:36:06 -0400

    First with gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import './App.css'
+import { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
 
 function App() {
 
-  return <div className='text-[30px]'>Hello World</div>
+const boxRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    gsap.to(boxRef.current, {duration: 2, x: 100, rotation: 360});
+  }, []);
+
+  return <div ref={boxRef} className="box">Animated Box</div>;
 }
 
 export default App
