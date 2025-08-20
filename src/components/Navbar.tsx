342eb424e9995638c46d5ce49d6e010a63cfe71b
sebassil12 <anddysilva15@gmail.com>
2025-08-19 23:32:46 -0400

    ADD:timelines for animations and assets

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,31 @@
+import { useGSAP } from '@gsap/react';
 import { navLinks, type NavLink } from '../../constants/index';
-
+import gsap from 'gsap';
 export default function Navbar() {
+
+    useGSAP(() => {
+        // GSAP animations can be added here
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top',
+            }
+        });
+
+        navTween.fromTo('nav', {
+            backgroundColor:'transparent',
+        },{
+            backgroundColor: '#00000050',
+            backgroundFilter: 'blur(10px)',
+            duration: 1,
+            ease: 'power1.inOut',
+        })
+    });
   return (
     <nav>
         <div>
             <a href="#home" className="flex items-center gap-2">
+                <img src="/images/logo.png" alt="logo" className="w-8 h-8" />
                 <p>Velvet Pour</p>
             </a>
 
