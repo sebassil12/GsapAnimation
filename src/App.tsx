e2fdede57f0e22bfc50c37746a5a8477d728f4ad
sebassil12 <anddysilva15@gmail.com>
2025-08-20 23:27:45 -0400

    IMP: animations in letter of presentations

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { gsap } from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <main className="App">
       <Navbar />
-
+      <Hero />
     </main>
   )
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,71 @@
+import { useGSAP } from "@gsap/react"
+import { SplitText } from "gsap/all"
+import gsap from "gsap";
+
+function Hero() {
+    useGSAP(() => {
+        // GSAP animations can be added here
+        const heroSplit = new SplitText('.title', { type: 'words, chars' });
+        
+        const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
+
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.05
+        })
+
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.05,
+            delay: 0.5
+        });
+
+    }, []);
+
+  return (
+    <>
+    <section id="hero" className="noisy">
+        <h1 className="title">MOJITO</h1>
+        
+        <img 
+        src="/images/hero-left-leaf.png"
+        alt="left leaf"
+        className="left-leaf"
+        />
+
+        <img 
+        src="/images/hero-right-leaf.png"
+        alt="right leaf"
+        className="right-leaf"
+        />
+
+        <div className="body">
+            <div className="content">
+                <div className="space-y-5 hidden md:block">
+                    <p>Cool. Crisp. CLassic.</p>
+                    <p className="subtitle">
+                        Sip the Spirit <br /> of Summer
+                    </p>
+                </div>
+
+                <div className="view-cocktails">
+                    <p className="subtitle">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam provident odit vel facilis officia debitis recusandae fuga. Doloremque repudiandae obcaecati.
+                    </p>
+                    <a href="#cocktails">VIew Cocktails</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Hero
