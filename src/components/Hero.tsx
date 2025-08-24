2b7adf5cb451b36936ca1046c82ea794f35f2432
sebassil12 <anddysilva15@gmail.com>
2025-08-24 19:35:22 -0400

    IMP: section of coktails

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -106,7 +106,7 @@ function Hero() {
     <div className="video absolute inset-0">
         <video 
             ref={videoRef}
-            src="/videos/input.mp4" 
+            src="/videos/output.mp4" 
             muted 
             playsInline
             preload="auto"
