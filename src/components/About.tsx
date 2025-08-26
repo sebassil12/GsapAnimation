1de98133fde419fbf28ba4257141df71f34e51ff
sebassil12 <anddysilva15@gmail.com>
2025-08-25 22:57:51 -0400

    IMP: about section complete

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,45 @@
+import { useGSAP } from "@gsap/react"
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
+
 export default function About() {
+  useGSAP(() => {
+    const titleSplit = SplitText.create('#about h2', {
+      type: 'words'
+    })
+
+    const scrollTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#about',
+        start: 'top center',
+      }
+    })
+
+    scrollTimeline
+    .from(titleSplit.words, {
+      opacity: 0,
+      duration:1,
+      yPercent: 100,
+      ease: 'expo.out',
+      stagger: 0.02
+    })
+    .from('.top-grid div, .bottom-grid div', {
+      opacity: 0,
+      duration:1,
+      ease: 'power1.inOut',
+      stagger: 0.04
+    }, '-=0.5')
+  });
+
   return (
     <div id ="about">
       <div className="mb-16 md:px-0 px-5">
         <div className="content">
           <div className="md:col-span-8">
             <p className="badge">Best Cocktails</p>
-            <h2>Where every details matters <span className="text-white">-</span>
-            from muddle to garnage.
+            <h2>
+            Where every details matters <span className="text-white">-</span>
+            from muddle to garnish
             </h2>
           </div>
           <div className="sub-content">
@@ -25,6 +58,35 @@ export default function About() {
           </div>
         </div>
       </div>
+      <div className="top-grid">
+        <div className="md:col-span-3">
+          <div className="noisy"/>
+          <img src="/images/abt1.png" alt="grid-img-1"/>
+        </div>
+        
+        <div className="md:col-span-6">
+          <div className="noisy"/>
+          <img src="/images/abt2.png" alt="grid-img-2"/>
+        </div>
+
+        <div className="md:col-span-3">
+          <div className="noisy"/>
+          <img src="/images/abt5.png" alt="grid-img-5"/>
+        </div>
+      </div>
+
+      <div className="bottom-grid">
+          <div className="md:col-span-8">
+            <div className="noisy"/>
+            <img src="/images/abt3.png" alt="grid-img-3"/>
+          </div>
+
+          <div className="md:col-span-4">
+            <div className="noisy"/>
+            <img src="/images/abt4.png" alt="grid-img-4"/>
+          </div>
+      </div>
+
     </div>
   )
 }
